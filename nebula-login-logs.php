33d7b5142e6f899fc64ca9fc14841d26eded5639
Katrine-Marie <fjordscene@gmail.com>
2019-02-12 10:04:09 +0100

    Set up plugin file header

---
+++ nebula-login-logs.php
@@ -0,0 +1,16 @@
+<?php
+/**********
+* Plugin Name: Nebula Login Logs
+* Description: Log all user logins, and display for administrators.
+* Version: 1.0.0
+* Author: Katrine-Marie Burmeister
+* Author URI: https://fjordstudio.dk
+* License: GPL-2.0+
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+*/
+
+namespace nebula\logging;
+
+if(!defined('ABSPATH')){
+	exit('Go away!');
+}
